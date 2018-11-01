a39c1c6790562a59e465da158892392e6d2123c0
frecklee <axcv99@gmail.com>
2018-11-01 09:42:05 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,39 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/ex">Ex</router-link>
+      <!-- <p style="font-family: 'Noto Sans KR', sans-serif;">안녕하셍</p> -->
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  @import url('https://fonts.googleapis.com/css?family=Arimo|Noto+Sans+KR|Bangers');
+  #app {
+    /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  
+  #nav {
+    padding: 10px;
+        font-family: 'Bangers', cursive;
+font-size: 25px;
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  }
+  
+  #nav a {
+    /* font-family: 'Noto Sans KR', sans-serif; */
+    font-weight: bold;
+    color: #2c3e50;
+  }
+  
+  #nav a.router-link-exact-active {
+    /* font-family: 'Noto Sans KR', sans-serif; */
+    color: #42b983;
+  }
 </style>
---
+++ src/views/Ex.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="Ex">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <p>주로 자주 들어가는 링크입니다.</p>
+    <ul>
+      <li><a href="https://vuejs.org/">vue.js 공식 홈페이지</a></li>
+      <li><a href="https://kr.vuejs.org/">vue.js 한국어 홈페이지</a></li>
+      <li><a href="https://gitter.im/vuejs/vue?utm_source=share-link&amp;utm_medium=link&amp;utm_campaign=share-link">vue.js 공식 채팅</a></li>
+      <li><a href="https://vuejs-korea.signup.team/">vue.js 한국어 사용자 채팅</a></li>
+      <li><a href="https://github.com/vuejs">vue.js 공식 조직</a></li>
+      <li><a href="https://github.com/vuejs-kr">vue.js 한국어 조직</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue">awesome vue</a></li>
+      <li><a href="http://forum.vuejs.org/">vue.js 포럼</a></li>
+    </ul>
+    <p>{{ greeting }} World!</p>
+    <p>{{ greeting2 }} World!</p>
+    <button style="text-align:center" type="submit" class="btn btn-success">제출</button>
+  </div>
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Arimo|Noto+Sans+KR');
+  
+  .Ex {
+    font-family: 'Noto Sans KR', sans-serif;
+    align-content: center;
+    text-align: center;
+  }
+  input, button {
+    border-radius: 30% !important;
+  }
+</style>
+<script>
+  module.exports = {
+    data: function() {
+      return {
+        greeting: 'Hello',
+        greeting2: 'bye'
+      }
+    },
+  }
+</script>
